3be4ec7dda7893540ebdfa5d77163f728685a924
guevarch <107594143+guevarch@users.noreply.github.com>
2023-08-14 19:25:53 -0400

    add

--- Grade C/static/js/logic.js
+++ Grade C/static/js/logic.js
@@ -1,3 +1,7 @@
+// MISSING LEGEND -10 and 
+// MISSING Each point has a tooltip with the Magnitude, the location and depth -10
+
+
 // We create the tile layer that will be the background of our map.
 console.log("Step 1 working");
 
@@ -78,12 +82,12 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
     // We create a popup for each marker to display the magnitude and location of the earthquake after the marker has been created and styled
     onEachFeature: function (feature, layer) {
       layer.bindPopup(
-        "Magnitude: "
-        + feature.properties.mag
-        + "<br>Depth: "
-        + feature.geometry.coordinates[2]
-        + "<br>Location: "
-        + feature.properties.place
+        // "Magnitude: "
+        // + feature.properties.mag
+        // + "<br>Depth: "
+        // + feature.geometry.coordinates[2]
+        // + "<br>Location: "
+        // + feature.properties.place
       );
     }
   }).addTo(map);
@@ -97,15 +101,15 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
   legend.onAdd = function () {
     let div = L.DomUtil.create("div", "info legend");
 
-    let grades = [-10, 10, 30, 50, 70, 90];
-    let colors = [
-      "#98ee00",
-      "#d4ee00",
-      "#eecc00",
-      "#ee9c00",
-      "#ea822c",
-      "#ea2c2c"
-    ];
+    // let grades = [-10, 10, 30, 50, 70, 90];
+    // let colors = [
+    //   "#98ee00",
+    //   "#d4ee00",
+    //   "#eecc00",
+    //   "#ee9c00",
+    //   "#ea822c",
+    //   "#ea2c2c"
+    // ];
 
     // Looping through our intervals to generate a label with a colored square for each interval.
     for (let i = 0; i < grades.length; i++) {
